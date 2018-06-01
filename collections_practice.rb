7119618214be901fef48ee6afe9038d93c87441c
Adam Urbina <adamurbina@gmail.com>
2018-06-01 01:39:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,8 +6,8 @@ def sort_array_desc(array)
     array.sort { |a,b| b <=> a}
 end
 
-def sort_array_char_count
-
+def sort_array_char_count(array)
+    array.sort { |a, b| a.size <==> b.size}
 end
 
 def swap_elements
