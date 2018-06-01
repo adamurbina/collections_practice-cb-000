515d399c869d6e974e681a63c65300cf1293c3ab
Adam Urbina <adamurbina@gmail.com>
2018-06-01 01:33:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,35 @@
 def sort_array_asc(array)
     array.sort
 end
+
+def sort_array_desc
+    array.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count
+
+end
+
+def swap_elements
+
+end
+
+def reverse_array
+
+end
+
+def kesha_maker
+
+end
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def add_s
+
+end
